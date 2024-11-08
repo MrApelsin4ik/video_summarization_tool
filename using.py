93cc01b800f9d563f14c801537538343133b7494
Nikita <54770094+MrApelsin4ik@users.noreply.github.com>
2024-11-08 15:54:15 +0300

    Update using.py

--- using.py
+++ using.py
@@ -3,22 +3,22 @@ import time
 
 BASE_URL = "http://127.0.0.1:8000" #change to your server ip
 
-# Пример данных для запроса суммаризации
+# Example data for summarization
 data1 = {
     "api_key": "", # yandex disk api key. You can get it here https://oauth.yandex.ru/authorize?response_type=token&client_id=eebf0e9b08e8464a83ba35ae5c746cf5
-    "video_path": "/Вторая мировая война.mp4",  # полный путь до видео
+    "video_path": "/Вторая мировая война.mp4",  # full path to video
 
 }
 
 data2 = {
-    "video_url": "https://disk.yandex.ru/i/t_qbkVAf-q1Egw",  # ссылка на видео
+    "video_url": "https://disk.yandex.ru/i/t_qbkVAf-q1Egw",  # url to video (if the video is shared)
 }
 
 data = data1 # or data2
 
 def submit_summarization_task(data):
     """
-    Отправляет запрос на суммаризацию видео и возвращает ID задачи.
+    Sends summarization request and return id of task
     """
     response = requests.post(f"{BASE_URL}/summarize/", json=data)
     if response.status_code == 200:
@@ -32,7 +32,7 @@ def submit_summarization_task(data):
 
 def check_task_status(task_id):
     """
-    Проверяет статус задачи по ID.
+    Checks task status by id
     """
     response = requests.get(f"{BASE_URL}/status/{task_id}")
     if response.status_code == 200:
@@ -46,7 +46,7 @@ def check_task_status(task_id):
 
 def get_summarization_result(task_id):
     """
-    Получает результат суммаризации по ID задачи.
+    Gets result of summarization by id
     """
     response = requests.get(f"{BASE_URL}/result/{task_id}")
     if response.status_code == 200:
